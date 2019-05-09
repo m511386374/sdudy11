94d527286dc25c6459175dd5e87319f22469173a
NickMing <511386374@qq.com>
2019-05-09 11:57:41 +0800

    Initial commit

---
+++ src/main/java/com/example/sdudy11/Sdudy11Application.java
@@ -0,0 +1,15 @@
+package com.example.sdudy11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Sdudy11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdudy11Application.class, args);
+    }
+
+}
